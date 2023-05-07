cf8799cbbfb6d65701eb874821154db7c446bfb6
1nf053c <1nf053c>
2023-05-07 16:29:56 -0500

    update

--- src/services/translate-hugo-file/translate-hugo-file.js
+++ src/services/translate-hugo-file/translate-hugo-file.js
@@ -1,20 +1,15 @@
-const { createCstToFileVisitor, createTranslatedCstVisitor } = require('./create-hugo-parser');
 const { parseHugo } = require('./parse-hugo');
 
-async function translateFileText(text) {
-    // parse text into cst
+async function translateHugoFileText(text) {
     const { results, translationIndices } = await parseHugo(text);
-    // return { results, trans
-    //return inputTree;
+    const translatedResults = await translate(results, translationIndices);
+    // const translatedFileText await resultsToFile(results);
+    
+    return translateHugoFileText;
+}
 
-    // translate tree
-    // const translateCstVisitor = createTranslatedCstVisitor(BaseCstVisitorConstructor);
-    // const translatedCst = translateCstVisitor.visit(inputTree);
-
-    // reconstruct text
-    // const cstToFileVisitor = createCstToFileVisitor(BaseCstVisitorConstructor);
-    // const translatedFileText = await cstToFileVisitor.visit(translatedCst); // create file from translated tree
-    //return translatedCst; // translatedFileText;
+async function translate(results, translationIndices){
+    // todo
 }
 
 module.exports = {
