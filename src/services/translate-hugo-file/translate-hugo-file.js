ba006deaf81fe94ccda79c1cec844f7752804626
1nf053c <1nf053c>
2023-05-07 16:51:08 -0500

    update

--- src/services/translate-hugo-file/translate-hugo-file.js
+++ src/services/translate-hugo-file/translate-hugo-file.js
@@ -1,15 +1,23 @@
 const { parseHugo } = require('./parse-hugo');
+const { translateText } = require('../translate-text');
+const { LANGUAGE_IDS } = require('../../assert-valid-language-id');
 
 async function translateHugoFileText(text) {
     const { results, translationIndices } = await parseHugo(text);
     const translatedResults = await translate(results, translationIndices);
-    // const translatedFileText await resultsToFile(results);
-    
-    return translateHugoFileText;
+    const translatedFileText = await asOneString(translatedResults);
+    return translatedFileText;
 }
 
-async function translate(results, translationIndices){
-    // todo
+async function translate(results, translationIndices) {
+    translationIndices.forEach(async i => {
+        results[i] = await translateText(results[i], LANGUAGE_IDS.es);
+    });
+    return results
+}
+
+async function asOneString(results) {
+    return results.join('')
 }
 
 module.exports = {
